c28af23d0758dff2199bc12f135397cc2070dcf9
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-04-29 19:22:19 -0700

    Update The-min-and-max-difference.js

--- The-min-and-max-difference.js
+++ The-min-and-max-difference.js
@@ -24,3 +24,10 @@ Examples:
 
 
 // Solution
+
+function maxAndMin(arr1,arr2){
+  return [
+    Math.max( ...arr1.map(n => Math.max( ...arr2.map(m => Math.abs(n - m)) )) ),
+    Math.min( ...arr1.map(n => Math.min( ...arr2.map(m => Math.abs(n - m)) )) )
+  ]
+}
